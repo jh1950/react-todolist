e90c3794911e46393a6eb1ad96531c758d673b6f
jh1950 <rlawnsgl191@gmail.com>
2025-03-27 00:08:16 +0900

    disabled bubble event

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -5,12 +5,19 @@ type ButtonProps = React.ComponentPropsWithoutRef<"button">;
 export default function Button({
 	className="",
     type="button",
+	onClick,
 	...props
 }: ButtonProps) {
+	const click = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+		e.stopPropagation();
+		if (onClick) onClick(e);
+	};
+
 	return (
 		<button
 			className={`${className} px-2.5 py-0.75 rounded-md font-semibold`.trim()}
             type={type}
+			onClick={click}
 			{...props}
 		/>
 	);
--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -5,12 +5,19 @@ type InputProps = React.ComponentPropsWithoutRef<"input">;
 export default function Input({
 	className="",
 	autoComplete="off",
+	onClick,
 	...props
 }: InputProps) {
+	const click = (e: React.MouseEvent<HTMLInputElement, MouseEvent>) => {
+		e.stopPropagation();
+		if (onClick) onClick(e);
+	};
+
 	return (
 		<input
 			className={`${className} outline-0 transition border-b-1 focus:border-theme-primary placeholder-(--text-color)/50`.trim()}
 			autoComplete={autoComplete}
+			onClick={click}
 			{...props}
 		/>
 	);
