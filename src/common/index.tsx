49364c8b9f3e4797e42a3b5e7d550a8f732c6462
jh1950 <rlawnsgl191@gmail.com>
2025-03-27 00:06:00 +0900

    add toTimeString

---
+++ src/common/index.tsx
@@ -0,0 +1,1 @@
+export { default as toTimeString } from "./toTimeString";
---
+++ src/common/toTimeString.tsx
@@ -0,0 +1,9 @@
+export default function toTimeString(timestamp: number, locales?: Intl.LocalesArgument, options: Intl.DateTimeFormatOptions = {}) {
+	const lang = locales || document.documentElement.getAttribute("lang") || "en";
+	return new Date(timestamp).toLocaleTimeString(lang, {
+		year: "numeric",
+		month: "2-digit",
+		day: "2-digit",
+		...options,
+	});
+};
